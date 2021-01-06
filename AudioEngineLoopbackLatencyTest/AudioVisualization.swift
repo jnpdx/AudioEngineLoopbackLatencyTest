ada9dd2e656ce8035a69779ee54c8558a3642ae0
jnpdx <john@johnnastos.com>
2021-01-06 14:10:46 -0800

    Improve drawing of waveform

--- AudioEngineLoopbackLatencyTest/AudioVisualization.swift
+++ AudioEngineLoopbackLatencyTest/AudioVisualization.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import Accelerate
 
-let kSamplesPerPixel : UInt32 = 50
+let kSamplesPerPixel : UInt32 = 40
 
 struct AudioVisualization: View {
     var graphData: [Float]
@@ -39,20 +39,34 @@ func convertAudioFileToVisualData(fileUrl: URL) -> ([Float],[Float]) {
     let samplesPerPixel = kSamplesPerPixel
     
     let pixelCount = Int(buffer.frameLength / samplesPerPixel)
-    let filter = [Float](repeating: 10.0 / Float(samplesPerPixel), count: Int(samplesPerPixel))
+    let filter = [Float](repeating: 1.0/Float(samplesPerPixel), count: Int(samplesPerPixel))
 
+    let bufferLength = vDSP_Length(buffer.frameLength)
+    
+    var processingBufferL = [Float](repeating: 0.0, count: Int(bufferLength))
+    var processingBufferR = [Float](repeating: 0.0, count: Int(bufferLength))
+    
+    //get absolute values to show magnitude
+    vDSP_vabs(buffer.floatChannelData![0], 1, &processingBufferL, 1, bufferLength)
+    vDSP_vabs(buffer.floatChannelData![1], 1, &processingBufferR, 1, bufferLength)
+    
+    //clip to range
+    var high:Float = 1.0
+    var low:Float = 0.0
+    vDSP_vclip(processingBufferL, 1, &low, &high, &processingBufferL, 1, bufferLength);
+    vDSP_vclip(processingBufferR, 1, &low, &high, &processingBufferR, 1, bufferLength);
     
     // downsample and average
     var downSampledDataL = [Float](repeating: 0.0, count: pixelCount)
     var downSampledDataR = [Float](repeating: 0.0, count: pixelCount)
         
-    vDSP_desamp(buffer.floatChannelData![0],
+    vDSP_desamp(processingBufferL,
                     vDSP_Stride(samplesPerPixel),
                     filter, &downSampledDataL,
                     vDSP_Length(pixelCount),
                     vDSP_Length(samplesPerPixel))
     
-    vDSP_desamp(buffer.floatChannelData![1],
+    vDSP_desamp(processingBufferR,
                     vDSP_Stride(samplesPerPixel),
                     filter, &downSampledDataR,
                     vDSP_Length(pixelCount),
